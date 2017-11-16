20a5466418aa3d0416510096f05e4d93086a1098
Chaudhari, Jaideo <Jaideo.Chaudhari@sony.com>
2017-11-16 15:06:47 -0800

    TUTORIAL #4 - State vs Props & Application Data

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,10 +4,18 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends React.Component {
+	constructor() {
+		super();
+		this.state = {"title": "Welcome"};		
+	}
 	render() {
+		// const title = "Test title";
+		setTimeout(() => {
+			this.setState({"title": "Welcome J"});
+		}, 2000);
 		return (
 			<div>
-				<Header/>
+				<Header title={this.state.title} />
 				<Footer/>
 			</div>	
 		);
